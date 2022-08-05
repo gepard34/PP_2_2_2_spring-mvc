6c16a536faf53bbea4c1cda4fc3af75d28146813
gepard34 <fhasjdfh@gmail.com>
2022-08-05 21:29:01 +0300

    1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+import web.service.CarServiceImpl;
+
+@Controller
+public class CarController {
+    private final CarService carService;
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String cars(@RequestParam(value = "count", required = false) Integer count, Model model){
+        model.addAttribute("cars", carService.index(count));
+        return "cars";
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,32 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+@Component
+public class CarServiceImpl implements CarService{
+    private List<Car> cars;
+    private static int CAR_COUNT;
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car(++CAR_COUNT, "Audi", "A6"));
+        cars.add(new Car(++CAR_COUNT, "BMW", "530"));
+        cars.add(new Car(++CAR_COUNT, "Lada", "2109"));
+        cars.add(new Car(++CAR_COUNT, "Mercedes", "E300"));
+        cars.add(new Car(++CAR_COUNT, "Toyota", "Camry"));
+        cars.add(new Car(++CAR_COUNT, "Nissan", "Skyline"));
+    }
+    @Override
+    public List<Car> index(Integer count) {
+        if ((count == null) || (count > cars.size())) {
+            return cars;
+        } else {
+            return cars.stream().limit(count).toList();
+        }
+    }
+
+
+}
